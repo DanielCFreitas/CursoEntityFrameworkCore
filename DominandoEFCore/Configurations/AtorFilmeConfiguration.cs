9369c88a323b4cbfe1f3b8363459bfeae833a9f4
Daniel Freitas <daniel.freitas@treevia.com.br>
2021-03-21 18:42:28 -0300

    Configuracoes de relacoes Muitos para Muitos, trocando nome dos campos criados fazendo uso do fluent api, adicionado propriedade a mais para a tabela de ligacao que e criada atraves do relacionamento de Muitos para Muitos

--- DominandoEFCore/Configurations/AtorFilmeConfiguration.cs
+++ DominandoEFCore/Configurations/AtorFilmeConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿using DominandoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
 
 namespace DominandoEFCore.Configurations
 {
@@ -8,10 +10,23 @@ namespace DominandoEFCore.Configurations
     {
         public void Configure(EntityTypeBuilder<Ator> builder)
         {
+            /*builder
+                .HasMany(p => p.Filmes)
+                .WithMany(p => p.Atores)
+                .UsingEntity(p => p.ToTable("AtoresFilmes"));*/
+
             builder
                 .HasMany(p => p.Filmes)
                 .WithMany(p => p.Atores)
-                .UsingEntity(p => p.ToTable("AtoresFilmes"));
+                .UsingEntity<Dictionary<string, object>>(
+                    "FilmesAtores",
+                    p => p.HasOne<Filme>().WithMany().HasForeignKey("FilmeId"),
+                    p => p.HasOne<Ator>().WithMany().HasForeignKey("AtorId"),
+                    p =>
+                    {
+                        p.Property<DateTime>("CadastradoEm").HasDefaultValueSql("CURRENT_DATE");
+                    }
+                );
         }
     }
 }
